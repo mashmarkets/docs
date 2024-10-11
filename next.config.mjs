77fa4bf3258857411dc91992e607cbeaa772de0d
jsonmaxi <183571481+jsonmaxi@users.noreply.github.com>
2024-10-11 21:32:33 +0800

    changed to static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,20 @@
 import nextra from "nextra";
 
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true
+  },
+  distDir: "out"
+}
+
 const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
   latex: true,
 });
 
-export default withNextra();
+export default withNextra(nextConfig);
