58fd51921ec3b399a33f49f1d70814127b79c4c0
kosong0x <kosong0x@proton.me>
2024-12-30 15:56:12 +0800

    Update _meta.ts

--- pages/_meta.ts
+++ pages/_meta.ts
@@ -1,9 +1,3 @@
 export default {
   index: "Home",
-  vision: "Vision",
-  "what-is-mash-markets": "What is Mash Markets?",
-  fees: "Fees",
-  trading: "Trading",
-  "revenue-share": "Revenue Share",
-  "technical-implementation": "Technical Implementation",
 };
